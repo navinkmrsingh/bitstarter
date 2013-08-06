f7d5d9144e4928559e6340d4f680d7cb2be19ece
Ubuntu <ubuntu@ip-172-31-38-8.us-west-2.compute.internal>
2013-08-06 04:40:10 +0000

    Changed index.html to fix alt bug

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
-var buf = fs.readFileSync('index.html','utf-8');
+var app = express(express.logger());
+var fle = 'index.html';
+var buf = fs.readFileSync(fle,'utf-8');
 var str = buf.toString();
 
 app.get('/', function(request, response) {
   response.send(str);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
